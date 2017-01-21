0105ef0adb15ae1fde59c78b0278a952d486a4ed
Thiago Andrade <thiagoh@gmail.com>
2017-01-20 22:00:48 -0500

    first commit

---
+++ Assignment1/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1 {
+    public partial class MainForm : Form {
+
+        private Exercise1 exercise1;
+        private Exercise2 exercise2;
+        private Exercise3 exercise3;
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (exercise1 == null) {
+                exercise1 = new Exercise1();
+            }
+            exercise1.Show();
+            exercise1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (exercise2 == null) {
+                exercise2 = new Exercise2();
+            }
+            exercise2.Show();
+            exercise2.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            if (exercise3 == null) {
+                exercise3 = new Exercise3();
+            }
+            exercise3.Show();
+            exercise3.Focus();
+        }
+    }
+}
